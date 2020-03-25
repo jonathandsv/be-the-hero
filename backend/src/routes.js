3da6734d16b71f048eda9a605d9cf393cea6bad8
Jonathan Vieira <jonathansouza59@gmail.com>
2020-03-25 01:22:22 -0300

    Back-End da aplicacao

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const OngController = require('./controlles/OngController');
+const IncidentController = require('./controlles/IncidentController');
+const ProfileController = require('./controlles/ProfileController');
+const SessionController = require('./controlles/SessionController');
+
+
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
